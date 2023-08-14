a16a518f4bcb03e0367713eed34a0b7f15abcc82
ernest-nde <ernestmekontso.codevibe@gmail.com>
2023-08-14 11:20:35 -0300

    initial commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import hamburger from "./assets/hamburger.svg";
+import close from "./assets/close.svg";
+
+
+export default function Navbar() {
+    const [showMenu, setShowMenu] = useState(false);
+    
+    return (
+        <div>
+        <h1>Navbar</h1>
+        </div>
+    )
+}
